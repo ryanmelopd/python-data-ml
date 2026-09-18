aa409bc0e78931b9368e57557371f6ff450fcf2f
Ryan <ryanmelopedro190@gmail.com>
2026-09-18 16:04:02 -0300

    feat: adiciona grafico de linha com matplotlib

--- 01-python-dados/exercicio_17.py
+++ 01-python-dados/exercicio_17.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 dataframe = pd.read_csv("01-python-dados/data/dados_jogos.csv")
 
 nomes = dataframe["nome"].tolist()
-precos = dataframe["preco"].tolist()
+avaliacoes = dataframe["avaliacao"].tolist()
 
-plt.bar(nomes, precos)
 
-plt.title("Preço dos jogos")
+plt.plot(nomes, avaliacoes)
+
+plt.title("Avaliações dos Jogos")
 plt.xlabel("Jogos")
-plt.ylabel("Preço")
-
+plt.ylabel("Avaliação")
 plt.show()
